3a43d196ef9f13aff92de2a2cad65e10e708d8cf
Zachary Pataky <zacharypataky@gmail.com>
2024-03-11 16:26:34 -0400

    DEVELOPMENT: Lesson 13: Add search functionality.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -19,8 +19,22 @@ export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
 
+  filteredLocationList: HousingLocation[] = [];
+
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filterResults(text: string): void {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(housingLocation =>
+      housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
   }
 
 }
