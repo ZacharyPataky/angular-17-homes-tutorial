6fb1bc9bb9fb72a5a33a546c1c29d88e7c6ce5b6
Zachary Pataky <zacharypataky@gmail.com>
2024-03-11 14:11:08 -0400

    DEVELOPMENT: Lesson 4: Create an interface.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
-import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { HousingLocation } from '../housing-location';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, HousingLocationComponent],
+  imports: [
+    CommonModule,
+    HousingLocationComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
 
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }
